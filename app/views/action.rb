6b810c887c35e76f95db31c86fc55ee0d3db2531
rberman6 <rittuberman@gmail.com>
2020-09-03 23:45:40 +0000

    compass two

--- app/views/action.rb
+++ app/views/action.rb
@@ -1,4 +1,9 @@
 get '/' do
-  File.read(File.join('app/views', 'index.html'))
+  # your previous finstagram post code is up here
+  # add the following @finstagram_posts line
+
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
   erb(:index)
 end
+
