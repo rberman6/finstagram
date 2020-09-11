7dcf38990d7e33c9ad16bbaa3ec162b273b5de3a
rberman6 <rittuberman@gmail.com>
2020-09-11 02:20:02 +0000

    add finstagram_post like and comments

--- app/views/action.rb
+++
@@ -1,9 +0,0 @@
-get '/' do
-  # your previous finstagram post code is up here
-  # add the following @finstagram_posts line
-
-  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-
-  erb(:index)
-end
-
